fe4d6b3dabe01912390e2eda3e3a494a8ea83bd8
Deepika Sidda <siddadeepika@gmail.com>
2025-07-07 16:25:21 +0530

    Initial commit

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'run install.sh: ' >> /home/ec2-user/app/deploy.log
+echo 'run after_install.sh: ' >> /home/ec2-user/app/deploy.log
 
 echo 'cd /home/ec2-user/app' >> /home/ec2-user/app/deploy.log
 cd /home/ec2-user/app
